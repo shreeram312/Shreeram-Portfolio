5b977c36d0b78b2b0c7f9951be6aa5caa9147d7d
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2024-11-25 13:38:59 +0530

    commit

--- src/sections/header.tsx
+++ src/sections/header.tsx
@@ -67,7 +67,7 @@ export default function Header() {
             className="relative border-2 border-slate-700 bg-opacity-85 group-hover:border-transparent bg-slate-900 text-white py-1 px-2 md:py-2 md:px-4 rounded-2xl text-sm md:text-base z-10 transition-all duration-300
         bg-clip-padding"
           >
-            Resume
+            Resume 
           </button>
         </div>
       </div>
